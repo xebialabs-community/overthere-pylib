53f671aef1b070fc217a3c902ada9c382cc6be0f
Ravan Naidoo <rnaidoo@xebialabs.com>
2014-12-11 12:02:51 +0100

    Preparation for releasing to central maven repository.
    -  Changed license
    -  Refactored package name
    -  Added signing and release tasks to build
    -  Added javadoc

--- src/main/java/com/xebialabs/overtherepy/DirectoryDiff.java
+++ src/main/java/com/xebialabs/overtherepy/DirectoryDiff.java
@@ -1,10 +1,27 @@
 /**
- * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
- * FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+ * Copyright (c) 2008-2014, XebiaLabs B.V., All rights reserved.
+ *
+ *
+ * Overtherepy is licensed under the terms of the GPLv2
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs Libraries.
+ * There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
+ * this software, see the FLOSS License Exception
+ * <http://github.com/xebialabs/overthere/blob/master/LICENSE>.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
+ * Floor, Boston, MA 02110-1301  USA
  */
 
-package com.xebialabs.deployit.plugin.otpylib;
+package com.xebialabs.overtherepy;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,12 +46,22 @@ import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
 import static com.google.common.collect.Sets.newHashSet;
 
+/**
+ * Compares 2 directories and determines which files were added, removed or changed.
+ */
 public class DirectoryDiff {
 
     private OverthereFile leftSide;
     private OverthereFile rightSide;
     private HashFunction hashFunction = Hashing.goodFastHash(32);
 
+
+    /**
+     * Constructor
+     *
+     * @param leftSide directory to compare
+     * @param rightSide directory to compare
+     */
     public DirectoryDiff(OverthereFile leftSide, OverthereFile rightSide) {
         checkArgument(leftSide.isDirectory(), "File [%s] must be a directory.", leftSide);
         checkArgument(rightSide.isDirectory(), "File [%s] must be a directory.", rightSide);
@@ -42,12 +69,25 @@ public class DirectoryDiff {
         this.rightSide = rightSide;
     }
 
+    /**
+     * Calculate the differences between the two directories that this class was constructed with.
+     *
+     * @return differences
+     * @throws IOException
+     */
     public DirectoryChangeSet diff() throws IOException {
         DirectoryChangeSet changeSet = new DirectoryChangeSet();
         compareDirectoryRecursive(leftSide, rightSide, changeSet);
         return changeSet;
     }
 
+    /**
+     * Calculate an MD5 hash for the given file.
+     *
+     * @param file for which MD5 should be calculated.
+     * @return MD5 hash
+     * @throws IOException
+     */
     public static String md5(final OverthereFile file) throws IOException {
         return ByteStreams.hash(new InputSupplier<InputStream>() {
             @Override
--- src/main/java/com/xebialabs/overtherepy/PyLoggerExecutionOutputHandler.java
+++ src/main/java/com/xebialabs/overtherepy/PyLoggerExecutionOutputHandler.java
@@ -1,10 +1,27 @@
 /**
- * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
- * FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+ * Copyright (c) 2008-2014, XebiaLabs B.V., All rights reserved.
+ *
+ *
+ * Overtherepy is licensed under the terms of the GPLv2
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs Libraries.
+ * There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
+ * this software, see the FLOSS License Exception
+ * <http://github.com/xebialabs/overthere/blob/master/LICENSE>.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
+ * Floor, Boston, MA 02110-1301  USA
  */
 
-package com.xebialabs.deployit.plugin.otpylib;
+package com.xebialabs.overtherepy;
 
 import java.io.Closeable;
 import java.util.Timer;
@@ -12,12 +29,15 @@ import java.util.TimerTask;
 
 import com.xebialabs.overthere.OverthereExecutionOutputHandler;
 
+/**
+ * Adapter that passes logging information from Overthere to a PyLogger implementation
+ */
 public class PyLoggerExecutionOutputHandler implements OverthereExecutionOutputHandler, Closeable {
 
     public static final int FLUSH_DELAY_MS = 5000;
     public static final int FLUSH_CHECK_INTERVAL_MS = 2000;
 
-    private static final Timer flushTimer = new Timer("AutoFlushTimer", true);
+    private final Timer flushTimer = new Timer("AutoFlushTimer", true);
 
     private PyLogger pyLogger;
     private boolean stdout;
